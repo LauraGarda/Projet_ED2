6337cf6f6a751d5f409c0b26be333924b745b36e
Laura <laura.garda@mines-paristech.fr>
2019-12-23 12:52:14 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -6,13 +6,24 @@ def solve_euler_explicit(f, x0, t0, tf, dt):
     t = t0
     x = x0
     while t < tf :
+        x = x + dt * f(t, x)
+        lx.append(x)
         t = t + dt
         lt.append(t)
-        x = x + dt * f(x)
-        lx.append(x)
     return (lt, lx)
 # j'ai rajouté t0 et tf pour savoir sur quel intervalle on résout l'équation
-# j'ai aussi supposé que f ne prend que un seul paramètre
 
 
 ## QUestion 2
+
+def solve_heun(f, x0, t0, tf, dt):
+    lt = [t0]
+    lx = [x0]
+    t = t0
+    x = x0
+    while t + dt < tf :
+        x = x + dt/2 * (f(t, x) + f(t+dt, x + dt * f(t, x)))
+        lx.append(x)
+        t = t + dt
+        lt.append(t)
+    return(lt, lx)
